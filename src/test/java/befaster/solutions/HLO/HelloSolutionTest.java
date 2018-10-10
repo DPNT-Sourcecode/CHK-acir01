ba338e133216dcb0835756a8516fa3e4fcc84806
sbx_user1060 <sbx_user1060@ip-10-128-179-177.eu-west-2.compute.internal>
2018-10-10 15:18:06 +0000

    Wed Oct 10 15:18:06 UTC 2018

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,4 +1,18 @@
 package befaster.solutions.HLO;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class HelloSolutionTest {
+    private HelloSolution helloSolution;
+
+    @Before
+    public void setUp(){
+        helloSolution = new HelloSolution();
+    }
+
+    @Test
+    public void say_hello(){
+
+    }
 }

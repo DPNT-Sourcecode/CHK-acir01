4bf972141d72c7f4e823fa503506079e4e19a35b
sbx_user1060 <sbx_user1060@ip-10-128-88-46.eu-west-2.compute.internal>
2018-10-11 09:54:45 +0000

    Thu Oct 11 09:54:45 UTC 2018

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -3,6 +3,9 @@ package befaster.solutions.CHK;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CheckoutSolutionTest {
 
     private CheckoutSolution checkoutSolution;
@@ -15,5 +18,13 @@ public class CheckoutSolutionTest {
     @Test
     public void get_price_a(){
         Integer price = checkoutSolution.checkout("2A");
+        assertThat(price, equalTo(100));
+
+        Integer price_special = checkoutSolution.checkout("3A");
+        assertThat(price_special, equalTo(130));
+
+        Integer price_over_special = checkoutSolution.checkout("5A");
+        //2 by default price and 3 for special
+        assertThat(price_over_special, equalTo(230));
     }
 }

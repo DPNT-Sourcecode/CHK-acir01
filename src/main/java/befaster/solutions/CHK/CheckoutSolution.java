00260cc30adb00a0815ba3371858f20fd0e2ae81
sbx_user1060 <sbx_user1060@ip-10-128-88-46.eu-west-2.compute.internal>
2018-10-11 09:27:45 +0000

    Thu Oct 11 09:27:45 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,7 +3,46 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
+    //offers table
+
+    // input 2B, 3A, etc...
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        //get amount
+        //get item
+        //check if item have special offers
+
+
+    }
+
+    private class Skus
+    {
+        private int amount;
+
+        // special amount
+        private int specialAmount;
+
+        // item
+        private String item;
+
+        private int price;
+        // is special
+        private boolean isSpecial;
+
+        // special price
+        private int specialPrice;
+
+        public int getTotalPrice(){
+            int totalPrice = 0;
+            //is have special price
+            if(isSpecial){
+                //calculate amount of special bundles
+                totalPrice = (amount / specialAmount) * specialPrice;
+                //add left item with default price
+                totalPrice += (amount % specialAmount) * price;
+                return totalPrice;
+            }
+            //else return amount by price
+            return amount * price;
+        }
     }
 }

d986925b68d4485c43ce0cb8d04459207a497241
sbx_user1059 <sbx_user1059@ip-10-129-238-175.eu-west-2.compute.internal>
2018-10-12 09:02:48 +0000

    Fri Oct 12 09:02:48 UTC 2018

--- src/main/java/befaster/solutions/CHK/SKUParser.java
+++ src/main/java/befaster/solutions/CHK/SKUParser.java
@@ -12,6 +12,28 @@ public class SKUParser {
     }
 
     public HashMap<String, SKU> parse() {
-        
+        //read line of file
+
+        //split on columns
+
+        //get item name
+
+        //get item price
+
+        //get special offers
+
+
+        return units;
     }
+
+    private void getSpecialOffers(String offersString) {
+        //offer type
+
+        //if reducer
+            //add reducer
+        //else
+            //add special
+    }
+
+
 }

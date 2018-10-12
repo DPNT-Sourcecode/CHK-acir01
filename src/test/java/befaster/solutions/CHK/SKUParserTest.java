8fccfb87d801144444858d9ac33fe82819105efb
sbx_user1059 <sbx_user1059@ip-10-129-238-175.eu-west-2.compute.internal>
2018-10-12 09:47:48 +0000

    Fri Oct 12 09:47:48 UTC 2018

--- src/test/java/befaster/solutions/CHK/SKUParserTest.java
+++ src/test/java/befaster/solutions/CHK/SKUParserTest.java
@@ -20,5 +20,11 @@ public class SKUParserTest {
         SKU result = skuParser.parseLine("| C    | 20    |                        |");
         assertThat(result.getItemName(), equalTo("C"));
         assertThat(result.getPrice(), equalTo(20));
+
+        SKU with_one_special = skuParser.parseLine("| B    | 30    | 2B for 45              |");
+        assertThat(with_one_special.getPrice(), equalTo(30));
+        assertThat(with_one_special.getItemName(), equalTo("B"));
+        assertThat(with_one_special.getSpecialOffersMap().containsKey(2), equalTo(true));
+        assertThat(with_one_special.getSpecialOffersMap().containsValue(45), equalTo(true));
     }
 }

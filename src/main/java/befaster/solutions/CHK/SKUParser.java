852db8938818b3e25514b48f308bec9303f26f45
sbx_user1059 <sbx_user1059@ip-10-129-238-175.eu-west-2.compute.internal>
2018-10-12 08:59:48 +0000

    Fri Oct 12 08:59:48 UTC 2018

---
+++ src/main/java/befaster/solutions/CHK/SKUParser.java
@@ -0,0 +1,17 @@
+package befaster.solutions.CHK;
+
+import java.util.HashMap;
+
+public class SKUParser {
+    private HashMap<String, SKU> units;
+    private String filePath;
+
+    public SKUParser(String filePath) {
+        this.filePath = filePath;
+        this.units = new HashMap<>();
+    }
+
+    public HashMap<String, SKU> parse() {
+        
+    }
+}
